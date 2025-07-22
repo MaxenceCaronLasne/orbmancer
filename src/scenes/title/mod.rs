1f7775dc7379d4c5674c9bfaa407b30533e6b940
Maxence Caron-Lasne <maxence.caronlasne@gmail.com>
2025-07-22 21:24:56 +0200

    add: title background

--- src/scenes/title/mod.rs
+++ src/scenes/title/mod.rs
@@ -1,16 +1,31 @@
+use agb::display::tiled::{
+    RegularBackground, RegularBackgroundSize, TileFormat, VRAM_MANAGER,
+};
+use agb::include_background_gfx;
 use agb::input::{Button, ButtonController};
 
 use crate::error::Error;
-use crate::save::Save;
 use crate::scenes::Scene;
+
+include_background_gfx!(
+    mod background,
+    "000000",
+    BACKGROUND => deduplicate "assets/title_screen.aseprite",
+);
 
 pub fn main(gba: &mut agb::Gba) -> Result<Scene, Error> {
     let mut gfx = gba.graphics.get();
     let mut input = ButtonController::new();
-    let mut timers = gba.timers.timers();
 
-    crate::bench::init(&mut timers);
     crate::reset::reset_input(Button::A, &mut input, &mut gfx);
+
+    VRAM_MANAGER.set_background_palettes(background::PALETTES);
+    let mut bg = RegularBackground::new(
+        agb::display::Priority::P0,
+        RegularBackgroundSize::Background32x32,
+        TileFormat::FourBpp,
+    );
+    bg.fill_with(&background::BACKGROUND);
 
     loop {
         input.update();
@@ -20,6 +35,7 @@ pub fn main(gba: &mut agb::Gba) -> Result<Scene, Error> {
         }
 
         let mut frame = gfx.frame();
+        bg.show(&mut frame);
         frame.commit();
     }
 }
