204a0b5cb37087b0db9fbb8b0d36e211ccb331e7
Maxence Caron-Lasne <maxence.caronlasne@gmail.com>
2025-08-12 21:41:28 +0200

    add: random ball selection in drop screen

--- src/save.rs
+++ src/save.rs
@@ -1,13 +1,18 @@
+use agb::{println, rng::RandomNumberGenerator};
 use heapless::Vec;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum BallKind {
-    Identity,
+    Identity = 0,
     TheDoubler,
     SmallGrabber,
 }
 
 impl BallKind {
+    pub const NB_DROPPABLES: usize = 2;
+    const DROPPABLES: [BallKind; BallKind::NB_DROPPABLES] =
+        [BallKind::TheDoubler, BallKind::SmallGrabber];
+
     pub fn sprite(&self) -> usize {
         match self {
             BallKind::Identity => 0,
@@ -22,6 +27,20 @@ impl BallKind {
             BallKind::TheDoubler => "The Doubler",
             BallKind::SmallGrabber => "The Small Grabber",
         }
+    }
+
+    pub fn from_i32(value: i32) -> BallKind {
+        match value {
+            0 => BallKind::Identity,
+            1 => BallKind::TheDoubler,
+            2 => BallKind::SmallGrabber,
+            _ => BallKind::Identity, // Default case
+        }
+    }
+
+    pub fn rand(rng: &mut RandomNumberGenerator) -> BallKind {
+        let v = rng.next_i32() as usize;
+        BallKind::DROPPABLES[v % BallKind::NB_DROPPABLES]
     }
 }
 
@@ -50,6 +69,11 @@ impl Save {
         }
     }
 
+    pub fn push_ball(&mut self, ball: BallKind) {
+        self.inventory.push(ball).unwrap_or_else(|_| {
+            println!("Full inventory");
+        });
+    }
 
     pub fn inventory(&self) -> &Vec<BallKind, 16> {
         &self.inventory
--- src/scenes/drop/mod.rs
+++ src/scenes/drop/mod.rs
@@ -1,11 +1,13 @@
 use agb::display::tiled::{
     RegularBackground, RegularBackgroundSize, TileFormat, VRAM_MANAGER,
 };
-use agb::include_background_gfx;
 use agb::input::{Button, ButtonController};
+use agb::rng::RandomNumberGenerator;
+use agb::{include_background_gfx, println};
+use const_random::const_random;
 
 use crate::error::Error;
-use crate::save::Save;
+use crate::save::{BallKind, Save};
 use crate::scenes::Scene;
 
 include_background_gfx!(
@@ -14,7 +16,9 @@ include_background_gfx!(
     BACKGROUND => deduplicate "assets/drop_screen.aseprite",
 );
 
-pub fn main(gba: &mut agb::Gba, _save: &mut Save) -> Result<Scene, Error> {
+const BENCH_SIZE: usize = 2;
+
+pub fn main(gba: &mut agb::Gba, save: &mut Save) -> Result<Scene, Error> {
     let mut gfx = gba.graphics.get();
     let mut input = ButtonController::new();
 
@@ -28,11 +32,31 @@ pub fn main(gba: &mut agb::Gba, _save: &mut Save) -> Result<Scene, Error> {
     );
     bg.fill_with(&background::BACKGROUND);
 
+    let mut rng = RandomNumberGenerator::new_with_seed([
+        const_random!(u32),
+        const_random!(u32),
+        const_random!(u32),
+        const_random!(u32),
+    ]);
+
+    let bench: [BallKind; BENCH_SIZE] =
+        [BallKind::rand(&mut rng), BallKind::rand(&mut rng)];
+    let mut selector: usize = 0;
+
     loop {
         input.update();
 
         if input.is_just_pressed(Button::A) {
+            save.push_ball(bench[selector]);
+            println!("pushed {:?}", bench[selector]);
             return Ok(Scene::Game);
+        }
+
+        if input.is_just_pressed(Button::LEFT) && selector > 0 {
+            selector -= 1;
+        }
+        if input.is_just_pressed(Button::RIGHT) && selector < BENCH_SIZE - 1 {
+            selector += 1;
         }
 
         let mut frame = gfx.frame();
